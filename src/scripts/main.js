aff78cf95610bc3d50f55b8fa0504ec9eaee6ece
Maksym Babalich <17033001m@gmail.com>
2026-08-07 09:47:28 +0300

    dolution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,253 @@
 'use strict';
 
-// write code here
+// Sort
+document.addEventListener('click', (e) => {
+  const tH = e.target.closest('th');
+
+  if (!tH) {
+    return;
+  }
+
+  const table = document.querySelector('table');
+  const tBody = table.tBodies[0];
+
+  const columnIndex = tH.cellIndex;
+
+  tH.classList.add('ASC');
+
+  const rows = [...tBody.rows];
+
+  if (tH.classList.contains('DESC')) {
+    rows.sort((rowA, rowB) => {
+      let valA = rowA.cells[columnIndex].textContent;
+      let valB = rowB.cells[columnIndex].textContent;
+
+      if (columnIndex === 0 || columnIndex === 1 || columnIndex === 2) {
+        return valB.localeCompare(valA);
+      }
+
+      if (columnIndex === 4) {
+        valA = Number(valA.replaceAll('$', '').replaceAll(',', ''));
+        valB = Number(valB.replaceAll('$', '').replaceAll(',', ''));
+
+        return valB - valA;
+      }
+
+      return Number(valB) - Number(valA);
+    });
+
+    tH.classList.remove('DESC');
+    tH.classList.add('ASC');
+
+    for (const row of rows) {
+      tBody.appendChild(row);
+    }
+  } else {
+    rows.sort((rowA, rowB) => {
+      let valueA = rowA.cells[columnIndex].textContent;
+      let valueB = rowB.cells[columnIndex].textContent;
+
+      if (columnIndex === 0 || columnIndex === 1 || columnIndex === 2) {
+        return valueA.localeCompare(valueB);
+      }
+
+      if (columnIndex === 4) {
+        valueA = Number(valueA.replaceAll('$', '').replaceAll(',', ''));
+        valueB = Number(valueB.replaceAll('$', '').replaceAll(',', ''));
+
+        return valueA - valueB;
+      }
+
+      return Number(valueA) - Number(valueB);
+    });
+    tH.classList.remove('ASC');
+    tH.classList.add('DESC');
+
+    for (const row of rows) {
+      tBody.appendChild(row);
+    }
+  }
+});
+
+// Add Active Rows
+document.addEventListener('click', (e) => {
+  const td = e.target.closest('td');
+
+  if (!td) {
+    return;
+  }
+
+  const row = td.closest('tr');
+  const rows = [...document.querySelector('table').rows];
+
+  for (let i = 0; i < rows.length; i++) {
+    if (rows[i].classList.contains('active')) {
+      rows[i].classList.remove('active');
+    }
+  }
+
+  row.classList.add('active');
+});
+
+function addForm() {
+  const body = document.querySelector('body');
+  const form = document.createElement('form');
+  const nameInput = ['Name', 'Position', 'Office', 'Age', 'Salary'];
+  const valueSelect = [
+    'Tokyo',
+    'Singapore',
+    'London',
+    'New York',
+    'Edinburgh',
+    'San Francisco',
+  ];
+
+  form.classList.add('new-employee-form');
+  body.appendChild(form);
+
+  for (let i = 0; i < nameInput.length; i++) {
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+
+    if (i === 2) {
+      const select = document.createElement('select');
+
+      label.append(nameInput[i] + ': ');
+      form.appendChild(label);
+      label.append(select);
+      select.required = true;
+      select.setAttribute('data-qa', 'office');
+      select.setAttribute('name', nameInput[i].toLowerCase());
+
+      for (let j = 0; j < valueSelect.length; j++) {
+        const option = document.createElement('option');
+
+        option.setAttribute('value', valueSelect[j]);
+        option.append(valueSelect[j]);
+
+        select.appendChild(option);
+      }
+      continue;
+    }
+
+    form.appendChild(label);
+    label.append(nameInput[i] + ': ');
+    label.appendChild(input);
+
+    if (nameInput[i] === 'Age' || nameInput[i] === 'Salary') {
+      input.setAttribute('name', nameInput[i].toLowerCase());
+      input.setAttribute('type', 'number');
+      input.setAttribute('data-qa', nameInput[i].toLowerCase());
+      input.required = true;
+      continue;
+    }
+
+    input.setAttribute('name', nameInput[i].toLowerCase());
+    input.setAttribute('type', 'text');
+    input.setAttribute('data-qa', nameInput[i].toLowerCase());
+    input.required = true;
+  }
+
+  const button = document.createElement('button');
+
+  button.textContent = 'Save to table';
+  button.setAttribute('type', 'submit');
+  form.appendChild(button);
+}
+
+function addNewPersonTable() {
+  const form = document.querySelector('form');
+  const body = document.querySelector('body');
+  const notification = document.createElement('div');
+  const title = document.createElement('h2');
+  const p = document.createElement('p');
+
+  title.classList.add('title');
+
+  notification.classList.add('notification');
+  notification.setAttribute('data-qa', 'notification');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (
+      Number(form.elements.age.value) < 18 ||
+      Number(form.elements.age.value) > 90
+    ) {
+      notification.className = 'notification';
+
+      notification.classList.add('error');
+      body.appendChild(notification);
+
+      title.textContent = 'Error';
+      p.textContent = 'Age less 18';
+      notification.appendChild(title);
+      notification.appendChild(p);
+
+      // setTimeout(() => {
+      //   notification.remove();
+      // }, 1000);
+
+      return;
+    }
+
+    if (form.elements.name.value.length < 4) {
+      notification.className = 'notification';
+
+      notification.classList.add('error');
+      body.appendChild(notification);
+
+      title.textContent = 'Error';
+      p.textContent = 'Name lenght less 4';
+      notification.appendChild(title);
+      notification.appendChild(p);
+
+      // setTimeout(() => {
+      //   notification.remove();
+      // }, 1000);
+
+      return;
+    }
+
+    const nameF = form.elements.name.value;
+    const position = form.elements.position.value;
+    const office = form.elements.office.value;
+    const age = Number(form.elements.age.value);
+    let salary = Number(form.elements.salary.value).toLocaleString('en-US');
+
+    salary = '$' + salary;
+
+    const valueForm = [nameF, position, office, age, salary];
+
+    const table = document.querySelector('table');
+    const tBody = table.tBodies[0];
+    const tr = document.createElement('tr');
+
+    tBody.appendChild(tr);
+
+    for (let j = 0; j < valueForm.length; j++) {
+      const td = document.createElement('td');
+
+      td.textContent = valueForm[j];
+
+      tr.appendChild(td);
+    }
+    notification.className = 'notification';
+    notification.classList.add('success');
+    body.appendChild(notification);
+
+    title.textContent = 'Success';
+    p.textContent = 'Employee added of table';
+    notification.appendChild(title);
+    notification.appendChild(p);
+
+    // setTimeout(() => {
+    //   notification.remove();
+    // }, 1000);
+
+    form.reset();
+  });
+}
+
+addForm();
+addNewPersonTable();
